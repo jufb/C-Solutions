9ba0fb08f6d306ca38cbef6e4557c0ac03eb818f
JuFB <fborges.j@gmail.com>
2021-11-11 16:18:12 -0500

    Changed the structure of the folders

---
+++ SolutionTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SolutionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            Console.WriteLine(Solution.TwoSum(nums,target));
+        }
+    }
+}

6ccae095ae2de44abc555848e348c2d9e8004f55
JuFB <fborges.j@gmail.com>
2022-03-24 21:55:46 -0400

    Included new operations Square Root and Fibonacci.

--- Calculator/Operations.cs
+++ Calculator/Operations.cs
@@ -2,21 +2,80 @@ namespace Calculator
 {
     public class Operations
     {
-        public float Sum(float v1, float v2) {
-            float result = v1 + v2;
-            return result;
+        public (decimal v1, decimal v2) Values {
+            get; set;
+        }
+        public decimal Result {
+            get; set;
+        }
+        public decimal Sum() {
+            Result = Values.v1 + Values.v2;
+            return Result;
         }
 
-        public float Subtract(float v1, float v2) {
-            float result = v1 - v2;
-            return result;
+        public decimal Subtract() {
+            Result = Values.v1 - Values.v2;
+            return Result;
         }
-        public float Divide(float v1, float v2) {
-            float result = v1 / v2;
-            return result;
+
+        public decimal Divide() {
+            Result = Values.v1 / Values.v2;
+            return Result;
         }
-        public float Multiply(float v1, float v2) {
-            float result = v1 * v2;
+        public decimal Multiply() {
+            Result = Values.v1 * Values.v2;
+            return Result;
+        }
+        public decimal SquareRoot(double v1) {
+            Result = (decimal)Math.Sqrt(v1);
+            return Result;
+        }
+        
+        ///<summary>
+        /// Count the Fibonacci sequence up to the informed number.
+        ///</summary>
+        public (int number, string sequence) Fibonacci(int n) {
+            int x = 0;
+            int y = 1;
+            int z = 0;
+            int number = n - 1; //because it starts at zero
+            string result = $"and the sequence up to {n} is: 0 1";
+            if (IsLowerThanTwo(n)) {
+                result = "Inform a higher number!";
+            }
+
+            for (int i = 2; i <= number; i++)
+            {
+                z = x + y; //0+1
+                result += $" {z}";
+                x = y; //1
+                y = z; //sum
+            }
+
+            return (z, result);
+        }
+
+        ///<summary>
+        /// Validates if the entered menu is between 1 and 4.
+        ///</summary>`
+        public Boolean IsCommonOperator(short e) {
+            bool valid = false;
+            if (e >= 1 && e <= 4)
+            {
+                valid = true;
+            }
+            return valid;
+        }
+        
+        ///<summary>
+        /// Validates if the entered number is lower than two or negative.
+        ///</summary>`
+        Boolean IsLowerThanTwo(int n)
+        {
+            bool result = false;
+            if(n <= 2 || n < 0) {
+                result = true;
+            }
             return result;
         }
     }
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,6 +7,9 @@
             Menu(false);
         }
 
+        ///<summary>
+        /// Calls the Menu of the application. Define 'False' if its the first call. Define 'True' to show greetings.
+        ///</summary>
         static void Menu(Boolean resp) {
             if(resp) {
                 Console.WriteLine("\nWhat operation would you like to do?\n");
@@ -20,21 +23,21 @@
             Console.WriteLine("2 - Subtract");
             Console.WriteLine("3 - Divide");
             Console.WriteLine("4 - Multiply");
+            Console.WriteLine("5 - Square Root");
+            Console.WriteLine("6 - Fibonacci");
             Console.WriteLine("0 - Exit");
             Console.WriteLine("__________________________________\n");
             
             try {
-                short e = short.Parse(Console.ReadLine());
+                string? e = Console.ReadLine();
 
-                if(e == 0) {
-                    Console.WriteLine("Thank you for using my Calculator!\n");
-                    System.Environment.Exit(0);
-                }
-                else if(e <= 4 ) {
-                    CalcArgs(e);
-                }
-                else {
-                    throw new Exception();
+                switch (e) {
+                    case "0": 
+                        Console.WriteLine("Thank you for using my Calculator!\n");
+                        System.Environment.Exit(0);
+                        break;
+                    case "1": case "2": case "3": case "4": case "5": case "6": CalcArgs(short.Parse(e)); break;
+                    default: throw new Exception();
                 }
             }
             catch (Exception) {
@@ -43,23 +46,38 @@
             }
         }
         static void CalcArgs(short e) {
-            Console.WriteLine("First value: ");
-            float v1 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Second value: ");
-            float v2 = float.Parse(Console.ReadLine());
-            float result = 0;
-
+            decimal v1 = 0;
+            decimal v2 = 0;
+            string sequence = "";
             Operations o = new Operations();
 
+            Console.WriteLine("Inform a value: ");
+
+            if (o.IsCommonOperator(e)) {
+                v1 = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Inform another value: ");
+                v2 = decimal.Parse(Console.ReadLine());
+                o.Values = (v1, v2);
+            }
+
             switch(e) {
-                case 1: result = o.Sum(v1, v2); break;
-                case 2: result = o.Subtract(v1, v2); break;
-                case 3: result = o.Divide(v1, v2); break;
-                case 4: result = o.Multiply(v1, v2); break;
+                case 1: o.Result = (decimal)o.Sum(); break;
+                case 2: o.Result = (decimal)o.Subtract(); break;
+                case 3: o.Result = (decimal)o.Divide(); break;
+                case 4: o.Result = (decimal)o.Multiply(); break;
+                case 5: 
+                    double root = double.Parse(Console.ReadLine());
+                    o.Result = (decimal)o.SquareRoot(root);
+                    break;
+                case 6: 
+                    int n = int.Parse(Console.ReadLine());
+                    Console.WriteLine(o.Fibonacci(n));
+                    (o.Result, sequence) = o.Fibonacci(n);
+                    break;
                 default: Menu(true); break;
             }
 
-            Console.WriteLine("The result is: " + result);
+            Console.WriteLine($"The result is: {o.Result} {sequence}");
             Menu(true);
         }
     }
